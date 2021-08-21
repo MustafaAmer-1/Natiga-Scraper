4ed44aba9b44ca6bdeee85c483c10758ead389f8
Mustafa Amer <1806185@eng.asu.edu.eg>
2021-08-21 05:55:58 +0200

    organize some code

--- natiga.py
+++ natiga.py
@@ -1,26 +1,32 @@
-import requests, csv, os, time
+import requests, csv, subprocess, time
 from bs4 import BeautifulSoup
+from json import loads
 
+# multithreading
 from queue import Queue
 from threading import Thread, Lock
 
+# google spreadsheet
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+
+start_seating = 1339993 # set the start_seating 
+end_seating = 10000000 # set the end_seating
+THREADS_NUM = 10
 
 scope = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/drive.file'
     ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_key.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_key.json', scope) # put your credentials in a json file named client_key.json
 client = gspread.authorize(creds)
-# sheet = client.open('all').sheet1
+
+SHEET_ID = '1PYewJ4oMt7-DoijLhseMGttaAxypJXa32ddIBDzxFJ4' # put your sheet id here
 
 url = 'https://natega.cairo24.com/Home/Result'
-start_seating = 1339993
-current_seating = start_seating
 
-csv_file = open('all_thread.csv', 'a')
+csv_file = open('grades.csv', 'a')
 csv_writer = csv.writer(csv_file)
 
 def get_data(seating):
@@ -60,9 +66,9 @@ class ProcessThread(Thread):
         self.ss = ss
     
     def run(self):
-        while self.ss < 10000000:
+        while self.ss < end_seating:
             row = get_data(self.ss)
-            self.ss += 10
+            self.ss += THREADS_NUM
             if(row):
                 addToQueue(row)
 
@@ -73,7 +79,7 @@ def is_threads_alive():
     return False
 
 threads = []
-for i in range(10):
+for i in range(THREADS_NUM):
     threads.append(ProcessThread(start_seating+i))
 
 for thread in threads:
@@ -95,21 +101,22 @@ while(is_threads_alive()):
     
     if(index % 10000 == 0):
         csv_file.flush()
-        res = os.system('curl -F "file=@all_thread.csv" https://file.io')
-        print(res)
-        r_f = open('all_thread.csv', 'r')
-        client.import_csv('1PYewJ4oMt7-DoijLhseMGttaAxypJXa32ddIBDzxFJ4', r_f.read().encode('utf-8'))        
+        res = subprocess.check_output('curl -F "file=@grades.csv" https://file.io', shell=True)
+        link = loads(res)['link']
+        print("The file uploaded at "  + link)
+        r_f = open('grades.csv', 'r')
+        client.import_csv(SHEET_ID, r_f.read().encode('utf-8'))        
         r_f.close()
 
 csv_file.close()
 print('\n---------------------------------------------Done!!---------------------------------------------\n')
 
-res = os.system('curl -F "file=@all_thread.csv" https://file.io')
+res = subprocess.check_output('curl -F "file=@grades.csv" https://file.io', shell=True)
 
-csv_file = open('all_thread.csv', 'r')
-client.import_csv('1PYewJ4oMt7-DoijLhseMGttaAxypJXa32ddIBDzxFJ4', csv_file.read().encode('utf-8'))
+csv_file = open('grades.csv', 'r')
+client.import_csv(SHEET_ID, csv_file.read().encode('utf-8'))
 csv_file.close()
 
 while(1):
-    print(res)
+    print("The file uploaded at "  + link)
     time.sleep(10)
